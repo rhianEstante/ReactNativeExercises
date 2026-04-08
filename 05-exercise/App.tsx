1a62e6252a14c82080d503fa1a4d609dec2647ee
Rhian Frances Estante <rhianesdev@gmail.com>
2026-04-08 22:14:08 +0200

    feat exercise 5

--- 05-exercise/App.tsx
+++ 05-exercise/App.tsx
@@ -1,11 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
+import { useState, useEffect } from 'react';
+
+type User = {
+  id: number,
+  name: String,
+  username: String,
+  email: String,
+  company: {
+    name: String
+  }
+  phone: String,
+  website: String
+}
 
 export default function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<String|null>(null);
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+      if (!response.ok) {
+        throw new Error("Error al obtener los usuarios")
+      }
+      const data = await response.json();
+      setUsers(data);
+    } catch (err) {
+      setError((err as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  if (loading) return <Text>Loading users...</Text>
+  if (error) return <Text>Error: {error}</Text>
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList  numColumns={2} data={users}
+      renderItem={({item}) => (
+        <View style={styles.card}>
+          <Text>{item.username}</Text>
+          <Text>{item.name}</Text>
+          <Text>{item.email}</Text>
+          <Text>{item.phone
+            }</Text>
+          <Text>{item.company.name}</Text>
+
+        </View>
+
+      )}
+      />
     </View>
   );
 }
@@ -14,7 +65,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  card :{
+    alignItems: 'center',
+    padding: 10,
+    margin: 5,
+    borderRadius: 20,
+    borderWidth: 1,
+    width: 500
+  }
 });
